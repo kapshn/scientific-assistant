048310a40a4a7e1867b61103e40758ea735d7a46
Igor <knzee@mail.ru>
2019-10-21 23:17:53 +0700

    v_secondpage

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,25 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 
-
-
-
-
-/* eslint-disable no-new */
-new Vue({
+var app = new Vue({
   el: '#app',
-  
-  
   render: h => h(App)
 })
-
-window.onload = function () {
-  document.getElementById('sign-in-button').addEventListener('click', function () {
-      chrome.identity.getAuthToken({ interactive: true }, function (token) {
-          // код работы с полученым токеном
-
-          //проверка получения токена
-          console.log("token: ", token);
-      });
-  });
-};
