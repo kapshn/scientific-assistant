152ea504e2bacbb0a2a455acc381969505712f89
Igor <knzee@mail.ru>
2019-11-16 20:21:05 +0700

    clear

--- src/mxgraph.js
+++
@@ -1,7 +0,0 @@
-import mxgraphFactory from 'mxgraph'
-
-export default mxgraphFactory({
-  // here should be your paths to mxgraph's assets (it's optional)
-  mxImageBasePath: 'assets/mxgraph/images',
-  mxBasePath: 'assets/mxgraph'
-})
